a33c4704e128caa9197be6c584d5bfee5df1e2d5
unknown <davidonzan@gmail.com>
2022-08-12 01:31:38 +0100

    Bug fixes

--- functions/traveller/public/js/layout.js
+++ functions/traveller/public/js/layout.js
@@ -6,3 +6,11 @@ auth.then(res => {
     document.getElementById("signup").style.display = 'block'
     document.getElementById("login_").style.display = 'block'
 })
+
+$(document).ready(function () {
+    async function logout(){
+        await catalyst.auth.signOut(document.URL)
+        .catch(err => console.log(err))
+    }
+    $("[logout]").click(logout)
+});
